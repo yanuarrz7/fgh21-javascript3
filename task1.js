51d3ead61caa8186d0181c00565f78b53802a018
Rizky Yanuar Ramadhan <yanuarrz71@gmail.com>
2024-06-07 19:40:39 +0700

    Add files via upload

--- task1.js
+++ task1.js
@@ -1,23 +1,34 @@
-const cekHariKerja = (day)=>{
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            const dataDay = ['senin', 'selasa', 'rabu', 'kamis', 'jumat']
-            let cek = dataDay.find((item)=>{
-                return item === day
-            })
-            if(cek){
-                resolve(cek)
-            }else{
-                reject(new Error('Hari ini bukan hari kerja'))
+const cekHariKerja = (day) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const dataDay = ['senin', 'selasa', 'rabu', 'kamis', 'jumat'];
+            let cek = dataDay.find((item) => item === day);
+            if (cek) {
+                resolve(cek);
+            } else {
+                reject(new Error('Hari ini bukan hari kerja'));
             }
-        },3000)
-    })
-}
+        }, 3000);
+    });
+};
 
-const hasil = new Promise(cekHariKerja)
-
-hasil.then((cek)=>{
-    console.log(cek)
-}).catch((error)=>{
+// then and catch
+cekHariKerja('jumat').then(function(hasil){
+    console.log(hasil)
+})
+.catch((error) => {
     console.log(error)
 })
+
+// try and catch
+const cekHari = async (day) => {
+    try {
+        const result = await cekHariKerja(day)
+        console.log(result)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+cekHari('senin')
+cekHari('minggu')
